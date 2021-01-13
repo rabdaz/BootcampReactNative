be173f17e7f6122fa2395cbd0e8fbddd5cc4d5f9
rabdaz <farabdaz.12@gmail.com>
2021-01-13 20:39:49 +0700

    update conditional.js

--- Tugas-2-String-Conditional/conditional.js
+++ Tugas-2-String-Conditional/conditional.js
@@ -30,55 +30,72 @@ var bulan =1;
 var tahun =1945;
 
 console.log("\nSoal Switch Case");
-if(tanggal >= 1&& tanggal <= 31){
-    if(bulan >= 1 && bulan <= 12){
-        if(tahun >= 1900 && tahun <= 2200){
-            switch(bulan){
-                case 1:
-                    console.log(tanggal+" Januari "+tahun);
-                    break;
-                case 2:
-                    console.log(tanggal+" Februari "+tahun);
-                    break;
-                case 3:
-                    console.log(tanggal+" Maret "+tahun);
-                    break;
-                case 4:
-                    console.log(tanggal+" April "+tahun);
-                    break;
-                case 5:
-                    console.log(tanggal+" Mei "+tahun);
-                    break;
-                case 6:
-                    console.log(tanggal+" Juni "+tahun);
-                    break;
-                case 7:
-                    console.log(tanggal+" Juli "+tahun);
-                    break;
-                case 8:
-                    console.log(tanggal+" Agustus "+tahun);
-                    break;
-                case 9:
-                    console.log(tanggal+" September "+tahun);
-                    break;
-                case 10:
-                    console.log(tanggal+" Oktober "+tahun);
-                    break;
-                case 11:
-                    console.log(tanggal+" November "+tahun);
-                    break;
-                case 12: 
-                    console.log(tanggal+" Desember "+tahun);
-                    break;
-            }
-        }else{
-            console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
-        }
-    }else{
-        console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
+switch (true){
+    case( tanggal < 1 || tanggal > 31):{
+        console.log('Data Error, range tanggal tidak tepat');
+        break;
     }
-
-}else{
-    console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
+    case( tahun < 1900 || tahun > 2200):{
+        console.log('Data Error, range tahun tidak tepat');
+        break;
+    }
+    case( bulan > 12 || bulan <1):{
+        console.log('Data Error, range bulan tidak tepat');
+        break;
+    }
+    default:{
+        switch(bulan){
+            case 1:
+                console.log(tanggal+" Januari "+tahun);
+                break;
+            case 2:
+                console.log(tanggal+" Februari "+tahun);
+                break;
+            case 3:
+                console.log(tanggal+" Maret "+tahun);
+                break;
+            case 4:
+                console.log(tanggal+" April "+tahun);
+                break;
+            case 5:
+                console.log(tanggal+" Mei "+tahun);
+                break;
+            case 6:
+                console.log(tanggal+" Juni "+tahun);
+                break;
+            case 7:
+                console.log(tanggal+" Juli "+tahun);
+                break;
+            case 8:
+                console.log(tanggal+" Agustus "+tahun);
+                break;
+            case 9:
+                console.log(tanggal+" September "+tahun);
+                break;
+            case 10:
+                console.log(tanggal+" Oktober "+tahun);
+                break;
+            case 11:
+                console.log(tanggal+" November "+tahun);
+                break;
+            case 12: 
+                console.log(tanggal+" Desember "+tahun);
+                break;
+        }
+    }
 }
+// if(tanggal >= 1&& tanggal <= 31){
+//     if(bulan >= 1 && bulan <= 12){
+//         if(tahun >= 1900 && tahun <= 2200){
+            
+//         }else{
+//             console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
+//         }
+//     }else{
+//         console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
+//     }
+
+// }else{
+//     console.log("Data Error, range tanggal/bulan/tahun tidak tepat");
+// }
 
